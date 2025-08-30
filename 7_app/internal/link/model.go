e7a0a27f8f2abef6de847fb9134d8c27804fd0aa
dwmdl <proph3tum@gmail.com>
2025-08-30 18:06:07 +0300

    feat(link) Add link model

---
+++ 7_app/internal/link/model.go
@@ -0,0 +1,31 @@
+package link
+
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func NewLink(url string) *Link {
+	return &Link{
+		Url:  url,
+		Hash: RandStringRunes(10),
+	}
+}
+
+var availableRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	randRunes := make([]rune, n)
+	for i := range randRunes {
+		randRunes[i] = availableRunes[rand.Intn(len(availableRunes))]
+	}
+
+	return string(randRunes)
+}
