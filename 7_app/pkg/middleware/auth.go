68469d6c6595ca6bf72711873766a1e7197f9bf3
dwmdl <proph3tum@gmail.com>
2025-11-25 09:03:34 +0300

    feat: Add jwt token validation

--- 7_app/pkg/middleware/auth.go
+++ 7_app/pkg/middleware/auth.go
@@ -3,20 +3,43 @@ package middleware
 import (
 	"api/configs"
 	"api/pkg/jwt"
-	"fmt"
+	"context"
 	"net/http"
 	"strings"
 )
 
+type key string
+
+const (
+	ContextEmailKey key = "ContextEmailKey"
+)
+
+func writeUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	_, err := w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+	if err != nil {
+		panic("WRITE_HEADER_ERROR")
+	}
+}
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
+		if !strings.HasPrefix(authHeader, "Bearer ") {
+			writeUnauthed(w)
+			return
+		}
+
 		token := strings.TrimPrefix(authHeader, "Bearer ")
 		isValid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		if !isValid {
+			writeUnauthed(w)
+			return
+		}
 
-		fmt.Println(isValid)
-		fmt.Println(data)
+		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
+		req := r.WithContext(ctx)
 
-		next.ServeHTTP(w, r)
+		next.ServeHTTP(w, req)
 	})
 }
