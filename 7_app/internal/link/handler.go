cd377dd086d0fac04a2b195cdcfcfb2b09991060
dwmdl <proph3tum@gmail.com>
2026-01-06 16:25:55 +0300

    fix: Add middleware for  link handler routes

--- 7_app/internal/link/handler.go
+++ 7_app/internal/link/handler.go
@@ -24,9 +24,9 @@ type Handler struct {
 
 func NewHandler(router *http.ServeMux, deps HandlerDeps) {
 	handler := &Handler{deps.LinkRepo, deps.EventBus}
-	router.HandleFunc("POST /link", handler.Create())
+	router.Handle("POST /link", middleware.IsAuthed(handler.Create(), deps.Config))
 	router.Handle("PATCH /link/{id}", middleware.IsAuthed(handler.Update(), deps.Config))
-	router.HandleFunc("DELETE /link/{id}", handler.Delete())
+	router.Handle("DELETE /link/{id}", middleware.IsAuthed(handler.Delete(), deps.Config))
 	router.HandleFunc("GET /{hash}", handler.GoTo())
 	router.Handle("GET /link", middleware.IsAuthed(handler.GetAll(), deps.Config))
 }
